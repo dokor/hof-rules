68757df1af55e7722f7fede4227a34a474ca3822
alelouet <alelouet@coreoz.com>
2023-07-27 15:05:04 +0200

    clean and implement SeasonList

--- frontend/src/components/features/hall-of-fame/HallOfFamePage.tsx
+++ frontend/src/components/features/hall-of-fame/HallOfFamePage.tsx
@@ -1,48 +1,17 @@
-import { getGlobalInstance } from 'plume-ts-di';
+import {getGlobalInstance} from 'plume-ts-di';
 import React from 'react';
-import ScoresService, { Season } from '../../../services/scores/ScoresService';
-import { UserProfile } from '../../../api/scores/ScoresApi';
-import UserTile from "./UserTile";
-import { useObservable } from 'micro-observables';
+import ScoresService, {Season} from '../../../services/scores/ScoresService';
+import SeasonList from "./SeasonList";
 
 export default function HallOfFamePage() {
   const scoresService = getGlobalInstance(ScoresService);
-  const firstSeason = useObservable(scoresService.fetchScores(Season.C_SCORE_SEASON_1));
-  const secondSeason = useObservable(scoresService.fetchScores(Season.C_SCORE_SEASON_2));
 
   return (
     <div className="hof-page">
       <button onClick={() => scoresService.refresh()}>Refresh ALL</button>
       <div className="hof-seasons">
-        <div className="hof-season">
-          <h1>Saison 1</h1>
-          <div>
-            {
-              firstSeason && firstSeason?.map(
-                (userProfile: UserProfile) => {
-                  return (
-                      <UserTile
-                          key={`firstSeason-${userProfile.slug}`}
-                          userProfile={userProfile}
-                      />
-                  )
-                }
-              )
-            }
-          </div>
-        </div>
-        <div className="hof-season">
-          <h1>Saison 2</h1>
-          <div>
-            {
-              secondSeason && secondSeason?.map(
-                (userProfile: UserProfile) => {
-                  return (<UserTile  key={`secondSeason-${userProfile.slug}`} userProfile={userProfile} />)
-                }
-              )
-            }
-          </div>
-        </div>
+        <SeasonList season={Season.C_SCORE_SEASON_1}/>
+        <SeasonList season={Season.C_SCORE_SEASON_2}/>
       </div>
     </div>
   );
--- frontend/src/components/features/hall-of-fame/UserTile.tsx
+++ frontend/src/components/features/hall-of-fame/UserTile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserProfile } from '../../../api/scores/ScoresApi';
+import {UserProfile} from '../../../api/scores/ScoresApi';
 import RankTile from './RankTile';
 
 type Props = {
@@ -9,26 +9,26 @@ type Props = {
 const URSER_URL_RULES = 'https://rules.art/user/';
 
 const computeProfileImage = (pictureUrl: string, fallbackUrl: string) => (
-    <img width={34} height={34}
-         src={pictureUrl}
-         onError={() => `this.onerror=null;this.src=${fallbackUrl}`}
-    />);
-export default function UserTile({ userProfile }: Props) {
+  <img width={34} height={34}
+       src={pictureUrl}
+       onError={() => `this.onerror=null;this.src=${fallbackUrl}`}
+  />);
+export default function UserTile({userProfile}: Props) {
   return (
     <div className="user-tile">
       <div className="user-tile-title">
-        <RankTile rank={userProfile.rank} />
+        <RankTile rank={userProfile.rank}/>
       </div>
       <div className="user-tile-content">
+        <div>
+          {computeProfileImage(userProfile.profile.pictureUrl, userProfile.profile.fallbackUrl)}
+        </div>
+        <div>
           <div>
-            {computeProfileImage(userProfile.profile.pictureUrl, userProfile.profile.fallbackUrl )}
+            <a href={URSER_URL_RULES + userProfile.slug} target="_blank" rel="noreferrer">{userProfile.username}</a>
           </div>
-          <div>
-              <div>
-                <a href={URSER_URL_RULES + userProfile.slug} target="_blank" rel="noreferrer">{userProfile.username}</a>
-              </div>
-              <div>Score : {userProfile.cscore}</div>
-          </div>
+          <div>Score : {userProfile.cscore}</div>
+        </div>
       </div>
     </div>
   );
