e446326879a8fa7b49e0744b7db115944307529d
Antoine Le Louet <alelouet@coreoz.com>
2023-07-31 14:51:41 +0200

    remove Appname on mobile

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,35 +1,19 @@
-import { MenuItem } from '@mui/material';
 import { getGlobalInstance } from 'plume-ts-di';
 import React from 'react';
-import { useObservable } from 'micro-observables';
-import LocaleService from '../../i18n/locale/LocaleService';
-import SessionService from '../../services/session/SessionService';
-import DropdownMenu from '../theme/DropdownMenu';
-import LocaleSelector from '../theme/LocaleSelector';
-import useMessages from '../../i18n/hooks/messagesHook';
-import Prices from "../features/hall-of-fame/Prices";
-import SearchComponent from "../features/hall-of-fame/SearchComponent";
+import Prices from '../features/hall-of-fame/Prices';
+import SearchComponent from '../features/hall-of-fame/SearchComponent';
 import ScoresService from '../../services/scores/ScoresService';
-
-function makeInitials(fullName?: string): string {
-  if (!fullName) {
-    return '';
-  }
-  const names = fullName.split(' ');
-  let initials = names[0].substring(0, 1).toUpperCase();
-
-  if (names.length > 1) {
-    initials += names[names.length - 1].substring(0, 1).toUpperCase();
-  }
-  return initials;
-}
+import EnvironmentService, { ScreenSize } from '../../services/utils/EnvironmentService';
+import { useObservable } from 'micro-observables';
 
 export default function Header() {
-  const scoresService = getGlobalInstance(ScoresService);
+  const scoresService: ScoresService = getGlobalInstance(ScoresService);
+  const environementService: EnvironmentService = getGlobalInstance(EnvironmentService);
+  const screenSize: ScreenSize = useObservable(environementService.screenSize());
 
   return (
     <header id="main-header">
-      <h1 className="section_name">Hof-rules by lelouet</h1>
+      {screenSize.isDesktop && <h1 className="section_name">Hof-rules by lelouet</h1>}
       <div className="header_actions">
         <div className="header_action">
           <Prices />
