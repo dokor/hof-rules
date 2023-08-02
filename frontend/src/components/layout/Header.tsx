cb1fa7f33b351faf9ff47b773a00fd551af6985d
Antoine Le Louet <alelouet@coreoz.com>
2023-08-02 15:05:46 +0200

    add lastTimeRefresh front

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -9,6 +9,7 @@ export default function Header() {
   const scoresService: ScoresService = getGlobalInstance(ScoresService);
   const environementService: EnvironmentService = getGlobalInstance(EnvironmentService);
   const screenSize: ScreenSize = useObservable(environementService.screenSize());
+  const lastTimeRefreshed: Date | undefined = useObservable(scoresService.getLastTimeRefreshed());
 
   return (
     <header id="main-header">
@@ -19,6 +20,7 @@ export default function Header() {
         </div>
         <div className="header_action">
           <button onClick={() => scoresService.refresh()}>Refresh ALL</button>
+          {lastTimeRefreshed && (<div>{lastTimeRefreshed.toUTCString()}</div>)}
         </div>
       </div>
     </header>
--- frontend/src/services/scores/ScoresService.ts
+++ frontend/src/services/scores/ScoresService.ts
@@ -1,4 +1,4 @@
-import { observable, WritableObservable } from 'micro-observables';
+import { Observable, observable, WritableObservable } from 'micro-observables';
 import ScoresApi, { UserProfile } from '../../api/scores/ScoresApi';
 import Season from '../../api/scores/types/Season';
 
@@ -7,13 +7,17 @@ export default class ScoresService {
 
   private readonly secondSesonScores: WritableObservable<UserProfile[]>;
 
+  private readonly lastTimeRefreshed: WritableObservable<Date | undefined>;
+
   constructor(
     private readonly scoresApi: ScoresApi,
   ) {
     this.firstSeasonScores = observable([]);
     this.secondSesonScores = observable([]);
+    this.lastTimeRefreshed = observable(undefined);
     this.init(Season.SEASON_1);
     this.init(Season.SEASON_2);
+    this.lastTimeRefreshed.set(new Date());
   }
 
   fetchScores(season: Season) {
@@ -34,21 +38,26 @@ export default class ScoresService {
 
   refresh() {
     this.scoresApi.refresh()
-      .then(() => {
+      .then((response: Date) => {
         this.init(Season.SEASON_1);
         this.init(Season.SEASON_2);
+        this.lastTimeRefreshed.set(response);
       });
   }
 
+  getLastTimeRefreshed(): Observable<Date | undefined> {
+    return this.lastTimeRefreshed.readOnly();
+  }
+
   getUserRank(slug: string, season: Season): UserProfile | undefined {
-    if (season === Season.SEASON_1) {
+    if (season === Season.SEASON_1 && this.firstSeasonScores.get() !== undefined) {
       const firstSeason: UserProfile | undefined = this.firstSeasonScores.get()
         .find((user: UserProfile) => user.slug === slug || user.username === slug);
       if (firstSeason) {
         return firstSeason;
       }
     }
-    if (season === Season.SEASON_2) {
+    if (season === Season.SEASON_2 && this.secondSesonScores.get() !== undefined) {
       const secondSeason: UserProfile | undefined = this.secondSesonScores.get()
         .find((user: UserProfile) => user.slug === slug || user.username === slug);
       if (secondSeason) {
